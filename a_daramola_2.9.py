3468b16eff2bec23f2aa159a1daf23c94c0a07a4
Nola360 <40280292+Nola360@users.noreply.github.com>
2022-07-18 17:04:03 -0400

    Add files via upload

---
+++ a_daramola_2.9.py
@@ -0,0 +1,21 @@
+#Akinola Daramola Jr
+#Programming Exercise 2.9
+#Due Date: 6/16/22
+
+
+
+#Declaring value of temperature
+temperature = 0.0
+
+#Redecalring value of temperature
+temperature = float(input('Enter a temperature in Celsius. '))
+
+#Declaring value of C - Celsius
+C = temperature
+
+#Declaring of value F - Farhenheit formula
+F = (9/5) * C + 32
+
+
+print(f"Degrees in Celsius: {temperature}°C")
+print(f"Degrees in Farhenheit: {F:.0f}°F")
